baab2ecb61975aeda38ec566314fe58b009ac157
Joseph <joseph@ahnga.net>
2018-11-03 13:01:43 -0700

    1st version of Hangman (No GUI)

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,5 @@
 // Choosing a random word
-var words = [
-    "javascript",
-    "monkey",
-    "amazing",
-    "pancake"
-];
+var words = ["mushroom", "flower", "castle", "princess"];
 var word = words[Math.floor(Math.random() * words.length)];
 
 // Creating the answer array
@@ -12,34 +7,72 @@ var answerArray = [];
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
 }
+
+// Creating the array for checking the letters whether have been used
+var alphabets = [];
+for (i = 0; i < 26; i++) {
+    alphabets[i] = true;
+}
+
 var remainingLetters = word.length;
+var remainingChances = 10;
+var userAnswers = "";
 
+var displayAnswer = document.getElementById("answers");
+var displayGuessed = document.getElementById("guesses")
+var userHP = document.getElementById("user");
+var comHP = document.getElementById("computer");    // .innerHTML
 
-// Game Loop
-while (remainingLetters > 0) {
+// 'On key up' function
+document.onkeyup = function (e) {
+    // Check the game status
+    if (remainingLetters ===0 || remainingChances === 0) {
+        return;
+    }
 
-    // Showing the progress
-    alert(answerArray.join(" "));
+    var userInput = e.key;
+    userInput = userInput.toLowerCase();
+    var codeInput = userInput.charCodeAt(0);
 
-    // Handling the player's input
-    var guess = prompt("Guess a letter, or click Cancel to stop playing.");
-    if (guess === null) {
-        break;
-    } else if (guess.length !== 1) {
-        alert("Please enter a single letter.");
-    } else {
+    // Valid only 'a~z' letters only
+    if (userInput.length === 1 && codeInput >= 97 && codeInput <= 122) {
+        // Check the input key whether have been used or not
+        if (alphabets[codeInput - 97]) {
+            alphabets[codeInput - 97] = false;
+            userAnswers += userInput.toUpperCase() + " ";
 
-        // Update the game state with the guess
-        for (var j = 0; j < word.length; j++) {
-            if (word[j] === guess) {
-                answerArray[j] = guess;
-                remainingLetters--;
+            var guessRight = false;
+
+            // Check with user input and the answer
+            for (i = 0; i < word.length; i++) {
+                if (word[i] === userInput) {
+                    guessRight = true;
+                    answerArray[i] = userInput;
+                    remainingLetters--;
+                }
+            }
+
+            if (!guessRight) {
+                remainingChances--;
+            }
+
+            var wordGuess = "";
+            for (var i = 0; i < answerArray.length; i++) {
+                wordGuess += answerArray[i] + " ";
+            }
+
+            // Display game status
+            displayAnswer.textContent = wordGuess;
+            displayGuessed.textContent = userAnswers;
+            userHP.textContent = remainingChances;
+            comHP.textContent = remainingLetters;
+
+            // Show ending
+            if (remainingLetters === 0) {
+                // Won
+            } else if (remainingChances === 0) {
+                // Lost
             }
         }
     }
-
 }
-
-// Ending the game
-alert(answerArray.join(" "));
-alert("Good job! The answer was " + word);
