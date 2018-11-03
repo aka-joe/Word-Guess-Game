58df52aaddcbb17405692ac794ff3567bd6e6344
Joseph <joseph@ahnga.net>
2018-11-03 13:37:29 -0700

    Fixed some Javascript to run better

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,9 +3,11 @@ var words = ["mushroom", "flower", "castle", "princess"];
 var word = words[Math.floor(Math.random() * words.length)];
 
 // Creating the answer array
+var wordGuess = "";
 var answerArray = [];
 for (var i = 0; i < word.length; i++) {
     answerArray[i] = "_";
+    wordGuess += "_ ";
 }
 
 // Creating the array for checking the letters whether have been used
@@ -16,12 +18,20 @@ for (i = 0; i < 26; i++) {
 
 var remainingLetters = word.length;
 var remainingChances = 10;
-var userAnswers = "";
+var userAnswers = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+var guessRight = false;
 
 var displayAnswer = document.getElementById("answers");
 var displayGuessed = document.getElementById("guesses")
 var userHP = document.getElementById("user");
-var comHP = document.getElementById("computer");    // .innerHTML
+var comHP = document.getElementById("computer");
+var endingGame = document.getElementById("message")    // .innerHTML
+
+// Display game status
+displayAnswer.textContent = wordGuess;
+displayGuessed.textContent = userAnswers;
+userHP.textContent = remainingChances;
+comHP.textContent = remainingLetters;
 
 // 'On key up' function
 document.onkeyup = function (e) {
@@ -39,11 +49,18 @@ document.onkeyup = function (e) {
         // Check the input key whether have been used or not
         if (alphabets[codeInput - 97]) {
             alphabets[codeInput - 97] = false;
-            userAnswers += userInput.toUpperCase() + " ";
 
-            var guessRight = false;
+            userAnswers = "";
+            for (i = 0; i < 26; i++) {
+                if (!alphabets[i]) {
+                    userAnswers += "* ";
+                } else {
+                    userAnswers += String.fromCharCode(i+65) + " ";
+                }
+            }
 
             // Check with user input and the answer
+            guessRight = false;
             for (i = 0; i < word.length; i++) {
                 if (word[i] === userInput) {
                     guessRight = true;
@@ -56,7 +73,7 @@ document.onkeyup = function (e) {
                 remainingChances--;
             }
 
-            var wordGuess = "";
+            wordGuess = "";
             for (var i = 0; i < answerArray.length; i++) {
                 wordGuess += answerArray[i] + " ";
             }
@@ -69,9 +86,9 @@ document.onkeyup = function (e) {
 
             // Show ending
             if (remainingLetters === 0) {
-                // Won
+                endingGame.textContent = "You won";
             } else if (remainingChances === 0) {
-                // Lost
+                endingGame.textContent = "You lost";
             }
         }
     }
